f748a3b9c1bf3e3cca5cf5f7d7730ad90dca57ba
Sri Surya Koushik Ande <sande@gitam.in>
2024-03-09 19:12:16 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './ToDo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoApp';
+
+  todos: Todo[] = [];
+  newTodo: string;
+  theme: boolean = false;
+  btn: any;
+
+  // Adding a todo into the todo list array
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo = "";
+    }else{
+      alert("Please Enter a ToDo");
+    }
+  }
+
+  // Complete-Restart a todo
+  Completetodo(id:number){
+    this.markTask(id).then((IDnum) => this.todos[IDnum].isCompleted = !this.todos[IDnum].isCompleted);
+  }
+  async markTask(id:number){
+    let result = id;
+    console.log(result);
+    return result; // returns a PROMISE, when fulfilled gives the "id" of the todo.
+  }
+
+  // Deleting a todo
+  Deletetodo(id:number){
+    this.dltTask(id).then((IDnum) => this.todos = this.todos.filter((v,i)=>i !== IDnum));
+  }
+  async dltTask(id:number){
+    let result = await id;  // using "await" converts the result into a value, or else an "async" function always returns a promoise.
+    console.log(result);
+    return result; 
+  }
+  
+
+  changeTheme(){
+    this.theme = !this.theme;
+    this.btn = document.getElementById("themeButton");
+    this.btn.addEventListener("click", () => {
+      const newTheme = (!this.theme)? "light" : "dark";
+      const newCta = newTheme === "dark" ? "dark" : "light";
+      this.btn.innerText = newCta;
+    });
+  }
 }
